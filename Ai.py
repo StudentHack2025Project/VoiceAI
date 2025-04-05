f26c69b7673e5586973856c3c9ae31d49f54ea74
ShahadAlharbi21 <shahad_100hr@outlook.com>
2025-04-05 17:44:03 +0100

    Added new library and did the motiviational task

--- Ai.py
+++ Ai.py
@@ -6,9 +6,12 @@ Created on Sat Apr  5 14:45:46 2025
 """
 
 import os
+import random
+import time
+import schedule
 from pyneuphonic import Neuphonic, TTSConfig
 from pyneuphonic.player import AudioPlayer
-def text_to_speech():
+def server_initiation():
     client = Neuphonic(api_key=os.environ.get('NEUPHONIC_API_KEY'))
     sse = client.tts.SSEClient() #Creating a connection between me and the server
     tts_config = TTSConfig(
@@ -18,12 +21,47 @@ def text_to_speech():
     ) #Voice_id is just to choose which voice we want
     voices = client.voices.list()
     #print(voices) to print out the list of voices so we gotta choose between them later
+    return tts_config, sse
+
+tts_config, sse = server_initiation()   
+def text_to_speech(text): 
     with AudioPlayer() as player:
-        response = sse.send('Hello Shahad I am here!', tts_config=tts_config)
+        response = sse.send(text, tts_config=tts_config)
         player.play(response)
-    
         player.save_audio('output.wav')  # save the audio to a .wav file
+        
+def motivational_prompts():
+    motivational_prompts_status = True#Need to adjust it and add a drop down menu from the main thing
+    motivational_prompts = [
+    "You're doing great, keep it up!",
+    "You're making awesome progress!",
+    "Keep going! You're almost there!",
+    "Great job! Now, let's focus on the next task.",
+    "Amazing effort! Keep pushing!",
+    "You’re on fire today, keep it up!",
+    "You’re staying focused! Let’s keep it going!"
+]
+    if motivational_prompts_status:
+        schedule.every(25).minutes.do(motivational_prompts_function,motivational_prompts)
+        
+def motivational_prompts_function(original_motivational_prompts):
+    sample_text = random.choice(original_motivational_prompts)
+    text_to_speech(sample_text)
+        
+
+motivational_prompts()
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
     
     
-text_to_speech()
+    
+    
+    
+        
+        
+        
+
+
     
